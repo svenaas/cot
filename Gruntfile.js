d5901ab802fb412b459f25511cf8b502d10246a7
Sven Aas <sven.aas@gmail.com>
2014-10-12 12:57:53 -0400

    Added cot theme. Regenerated themes using grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,6 +46,7 @@ module.exports = function(grunt) {
 					'css/theme/serif.css': 'css/theme/source/serif.scss',
 					'css/theme/simple.css': 'css/theme/source/simple.scss',
 					'css/theme/sky.css': 'css/theme/source/sky.scss',
+					'css/theme/cot.css': 'css/theme/source/cot.scss',
 					'css/theme/moon.css': 'css/theme/source/moon.scss',
 					'css/theme/solarized.css': 'css/theme/source/solarized.scss',
 					'css/theme/blood.css': 'css/theme/source/blood.scss'
